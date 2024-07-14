cac810d1b3b491dba39895d7dc805bc403d60408
jaz <zayas.j10@gmail.com>
2024-07-15 01:17:01 +0800

    init setup svelte

---
+++ src/lib/components/pages/home/home.svelte
@@ -0,0 +1,8 @@
+<script>
+	let x = $state(0);
+</script>
+
+<h1 class="text-red-400">Welcome to SvelteKit {x}</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<style></style>
