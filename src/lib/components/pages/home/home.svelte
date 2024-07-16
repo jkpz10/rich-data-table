22b9462dd4393e9c50072d07ce56bd0e04bb9d8d
jaz <zayas.j10@gmail.com>
2024-07-17 01:11:37 +0800

    fix: filter showing on bottom

--- src/lib/components/pages/home/home.svelte
+++ src/lib/components/pages/home/home.svelte
@@ -99,106 +99,105 @@
 			<p>{error}</p>
 		</div>
 	{:else}
-		<div class="relative overflow-x-auto shadow-md sm:rounded-lg pt-4 border">
-			<Datatable {table}>
-				{#snippet header()}
-					<div class="flex w-full justify-between items-center">
-						<div class="px-4">
-							<p class="text-lg font-semibold">User</p>
-						</div>
-						<div class="flex items-center justify-end gap-2 px-4">
-							<Search {search} setValue={() => search.set()} />
-							<button
-								id="dropdownRadioButton"
-								data-dropdown-toggle="dropdownRadio"
-								class="flex gap-1 items-center text-gray-500 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-3 py-2"
-								type="button"
-								onclick={() => (showFilter = !showFilter)}
-							>
-								Filter
-								<svg
-									width="21"
-									height="20"
-									viewBox="0 0 20 20"
-									fill="none"
-									xmlns="http://www.w3.org/2000/svg"
+		<div class="flex gap-2 w-full">
+			<div class="relative overflow-x-auto shadow-md rounded-md w-full pt-4 border items-center">
+				<Datatable {table}>
+					{#snippet header()}
+						<div class="flex w-full justify-between items-center">
+							<div class="px-4">
+								<p class="text-lg font-semibold">User</p>
+							</div>
+							<div class="flex items-center justify-end gap-2 px-4">
+								<Search {search} setValue={() => search.set()} />
+								<button
+									id="dropdownRadioButton"
+									data-dropdown-toggle="dropdownRadio"
+									class="flex gap-1 items-center text-gray-500 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-3 py-2"
+									type="button"
+									onclick={() => (showFilter = !showFilter)}
 								>
-									<path
-										d="M5 10H15M2.5 5H17.5M7.5 15H12.5"
-										stroke="#344054"
-										stroke-width="1.66667"
-										stroke-linecap="round"
-										stroke-linejoin="round"
-									/>
-								</svg>
-							</button>
+									Filter
+									<svg
+										width="21"
+										height="20"
+										viewBox="0 0 20 20"
+										fill="none"
+										xmlns="http://www.w3.org/2000/svg"
+									>
+										<path
+											d="M5 10H15M2.5 5H17.5M7.5 15H12.5"
+											stroke="#344054"
+											stroke-width="1.66667"
+											stroke-linecap="round"
+											stroke-linejoin="round"
+										/>
+									</svg>
+								</button>
+							</div>
 						</div>
-					</div>
-				{/snippet}
-				{#snippet footer()}
-					<div class="w-full flex justify-between items-center">
-						<div class="flex items-center gap-2">
-							<RowsPerPage {table} />
-							<RowCount {table} />
+					{/snippet}
+					{#snippet footer()}
+						<div class="w-full flex justify-between items-center">
+							<div class="flex items-center gap-2">
+								<RowsPerPage {table} />
+								<RowCount {table} />
+							</div>
+
+							<Pagination {table} />
 						</div>
+					{/snippet}
 
-						<Pagination {table} />
-					</div>
-				{/snippet}
-
-				<table class="w-full text-sm text-left rtl:text-right text-gray-500">
-					<thead class="text-xs text-gray-700 bg-[#F9FAFB]">
-						<tr>
-							<Th>
-								<input
-									id="checkbox-all-search"
-									class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500"
-									type="checkbox"
-									checked={table.isAllSelected}
-									onclick={() => table.selectAll({ scope: 'all' })}
-								/>
-								<label for="checkbox-all-search" class="sr-only">checkbox</label>
-							</Th>
-							<ThSort {table} field="first_name">First Name</ThSort>
-							<ThSort {table} field="last_name">Last Name</ThSort>
-							<ThSort {table} field="city">City</ThSort>
-							<ThSort {table} field="country">Country</ThSort>
-						</tr>
-					</thead>
-					<tbody>
-						{#each table.rows as row}
-							<tr
-								class:active={table.selected.includes(row.id)}
-								class="bg-white border-b hover:bg-[#F9FAFB]"
-							>
-								<td class="w-4 p-4">
+					<table class="w-full text-sm text-left rtl:text-right text-gray-500">
+						<thead class="text-xs text-gray-700 bg-[#F9FAFB]">
+							<tr>
+								<Th>
 									<input
-										class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2"
+										id="checkbox-all-search"
+										class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500"
 										type="checkbox"
-										checked={table.selected.includes(row.id)}
-										onclick={() => table.select(row.id)}
+										checked={table.isAllSelected}
+										onclick={() => table.selectAll({ scope: 'all' })}
 									/>
-								</td>
-								<td>{row.first_name}</td>
-								<td>{row.last_name}</td>
-								<td>{row.city}</td>
-								<td>{row.country}</td>
+									<label for="checkbox-all-search" class="sr-only">checkbox</label>
+								</Th>
+								<ThSort {table} field="first_name">First Name</ThSort>
+								<ThSort {table} field="last_name">Last Name</ThSort>
+								<ThSort {table} field="city">City</ThSort>
+								<ThSort {table} field="country">Country</ThSort>
 							</tr>
-						{/each}
-					</tbody>
-				</table>
-			</Datatable>
+						</thead>
+						<tbody>
+							{#each table.rows as row}
+								<tr
+									class:active={table.selected.includes(row.id)}
+									class="bg-white border-b hover:bg-[#F9FAFB]"
+								>
+									<td class="w-4 p-4">
+										<input
+											class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2"
+											type="checkbox"
+											checked={table.selected.includes(row.id)}
+											onclick={() => table.select(row.id)}
+										/>
+									</td>
+									<td>{row.first_name}</td>
+									<td>{row.last_name}</td>
+									<td>{row.city}</td>
+									<td>{row.country}</td>
+								</tr>
+							{/each}
+						</tbody>
+					</table>
+				</Datatable>
+			</div>
+			{#if showFilter}
+				<div
+					class="flex flex-col items-start justify-start border shadow-md rounded-md gap-3 p-4"
+				>
+					<Filter onSelect={handleFilterCity} title={'City'} options={cityOptions} />
+					<Filter onSelect={handleFilterCountry} title={'Country'} options={countryOptions} />
+				</div>
+			{/if}
 		</div>
 	{/if}
-	<Modal class="w-[500px]" title="Filter" bind:open={showFilter}>
-		<div class="flex flex-col items-start justify-center gap-3">
-			<Filter onSelect={handleFilterCity} title={'City'} options={cityOptions} />
-			<Filter onSelect={handleFilterCountry} title={'Country'} options={countryOptions} />
-		</div>
-		<svelte:fragment slot="footer">
-			<Button on:click={() => (showFilter = false)} class="border-0" color="alternative"
-				>Close</Button
-			>
-		</svelte:fragment>
-	</Modal>
 </div>
